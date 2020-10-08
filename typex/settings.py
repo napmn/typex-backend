6b76aedb4f23b48915592fc035497ff4ca31f6ea
napmn <lukass135@gmail.com>
2020-10-08 20:16:11 +0200

    :ambulance: removed secret key from settings

--- typex/settings.py
+++ typex/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c1(#=o5-cx%7c@sdk%uah5guw*3rx8d3d9o*fh1#++@@tf@1dn'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=True)
