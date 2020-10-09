a7051de7c213520dc7abd076c7faad48f8d69f71
napmn <lukass135@gmail.com>
2020-10-09 16:04:01 +0200

    :building_construction: quotes basic admin and basic user model

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from quotes.models import Author, Quote
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ('author', 'quote_type')
--- users/models.py
+++ users/models.py
@@ -1,15 +1,7 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-    PASSWORD_MIN_SIZE = 5
-
-    email = models.EmailField(_('email'), unique=True)
-
-    class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
+class User(AbstractUser):
+    pass
